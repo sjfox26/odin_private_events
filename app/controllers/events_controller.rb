409de75cded69bf6a41931fe7e9174ff210e5fef
Scott Fox <scottfox87@gmail.com>
2018-03-07 16:27:02 -0600

    fill in events controller

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,10 +1,22 @@
 class EventsController < ApplicationController
   def index
+    @events = Event.all
   end
 
   def new
+    @event = Event.new
+  end
+
+  def create
+    @event = current_user.created_events.build(event_params)
   end
 
   def show
+    @event = Event.find(params[:id])
   end
+
+    private
+    def event_params
+      params.require(:event).permit(:name, :date, :location, :description)
+    end
 end
