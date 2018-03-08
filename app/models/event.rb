603e0783c3a2504978ff0f6fe56ab63086d24d2a
Scott Fox <scottfox87@gmail.com>
2018-03-08 17:03:21 -0600

    added 'Previous' to events page, refactored upcoming and past methods into scopes

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,7 +2,10 @@ class Event < ApplicationRecord
   belongs_to :creator, class_name: "User"
   has_many :event_attendees, foreign_key: "attendee_event_id"
   has_many :attendees, through: "event_attendees", source: "event_attendee"
+  scope :upcoming, -> { where("date >= ?", Date.current).order(date: :asc) }
+  scope :past, -> { where("date < ?", Date.current).order(date: :desc) }
 
+=begin
   def self.upcoming
     where('date >= ?', DateTime.now)
   end
@@ -10,6 +13,8 @@ class Event < ApplicationRecord
   def self.past
     where('date < ?', DateTime.now)
   end
+=end
+
 
 #  def self.published             ###Example from tutorial online
 #    where(status: 'published')
